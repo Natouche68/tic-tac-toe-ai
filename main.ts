1a583d66b5a274af49125b253d0ab21a9032ecf8
Natouche68 <ngrossheny@gmail.com>
2023-05-27 19:50:23 +0200

    Make a basic arena renderer

--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
-import { stack } from "https://deno.land/x/colorify@1.0.5/mod.ts";
+import { crayon } from "https://deno.land/x/crayon@3.3.3/mod.ts";
 
-stack.log("blue", "cyan", "underlined", "Hello World !");
+type TicTacToeCase = " " | "x" | "o";
+
+const ticTacToeArena: TicTacToeCase[][] = [
+  [" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "],
+];
+
+console.log(crayon.bgBlue(crayon.white("                    ")));
+console.log(crayon.bgBlue(crayon.white("   Tic Tac Toe AI   ")));
+console.log(crayon.bgBlue(crayon.white("                    ")));
+
+console.log("");
+
+renderTicTacToeArena();
+
+function renderTicTacToeArena() {
+  console.log(
+    ` ${ticTacToeArena[0][0]} | ${ticTacToeArena[0][1]} | ${
+      ticTacToeArena[0][2]
+    } `,
+  );
+  console.log(`---+---+---`);
+  console.log(
+    ` ${ticTacToeArena[1][0]} | ${ticTacToeArena[1][1]} | ${
+      ticTacToeArena[1][2]
+    } `,
+  );
+  console.log(`---+---+---`);
+  console.log(
+    ` ${ticTacToeArena[2][0]} | ${ticTacToeArena[2][1]} | ${
+      ticTacToeArena[2][2]
+    } `,
+  );
+}
