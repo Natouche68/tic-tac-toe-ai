e6720c4801ae3a90e651c0f7c557174117378c18
Natouche68 <ngrossheny@gmail.com>
2023-06-03 12:28:31 +0200

    Create a basic AI

--- main.ts
+++ main.ts
@@ -5,6 +5,17 @@ const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 type TicTacToeCase = " " | "x" | "o" | "•";
 
+type AIPrediction = {
+  aiMove: {
+    y: number;
+    x: number;
+  };
+  arena: TicTacToeCase[][];
+  score: number;
+  depth: number;
+  completed: boolean;
+};
+
 const ticTacToeArena: TicTacToeCase[][] = [
   [" ", " ", " "],
   [" ", " ", " "],
@@ -16,6 +27,8 @@ let currentPlayer: "player" | "ai" = "player";
 
 let selectedCaseY = -1;
 let selectedCaseX = -1;
+
+let rankedAIPredictions: AIPrediction[] = [];
 
 renderTitle();
 
@@ -30,7 +43,7 @@ while (!isGameOver) {
     currentPlayer = "player";
   }
 
-  const winResult = checkIfWin();
+  const winResult = checkIfWin(ticTacToeArena);
 
   if (winResult === "o") {
     console.log("The player wins !");
@@ -206,11 +219,163 @@ async function chooseCasePlayer() {
 async function chooseCaseAI() {
   renderTitle();
 
-  await sleep(3000);
+  const aiPredictions: AIPrediction[] = [];
+
+  const emptyCases = getEmptyCases(ticTacToeArena);
+
+  for (let i = 0; i < emptyCases.length; i++) {
+    const newPrediction: AIPrediction = {
+      aiMove: {
+        y: emptyCases[i].y,
+        x: emptyCases[i].x,
+      },
+      arena: structuredClone(ticTacToeArena),
+      score: 0,
+      depth: 0,
+      completed: false,
+    };
+
+    newPrediction.arena[newPrediction.aiMove.y][newPrediction.aiMove.x] = "x";
+
+    if (checkIfWin(newPrediction.arena) == "x") {
+      newPrediction.completed = true;
+      newPrediction.score = 10 - newPrediction.depth;
+    }
+
+    aiPredictions.push(newPrediction);
+  }
+
+  predictPlayerMoves(aiPredictions, 8);
+
+  ticTacToeArena[rankedAIPredictions[0].aiMove.y][
+    rankedAIPredictions[0].aiMove.x
+  ] = "x";
+
+  renderTicTacToeArena();
+
+  await sleep(5000);
+
   return;
 }
 
-function checkIfWin(): " " | "o" | "x" {
+function getEmptyCases(arena: TicTacToeCase[][]): { x: number; y: number }[] {
+  const emptyCases: { x: number; y: number }[] = [];
+
+  arena.forEach((row, y) => {
+    row.forEach((ticTacToeCase, x) => {
+      if (ticTacToeCase === " ") {
+        emptyCases.push({
+          y,
+          x,
+        });
+      }
+    });
+  });
+
+  return emptyCases;
+}
+
+function predictPlayerMoves(
+  aiPredictions: AIPrediction[],
+  predictionDepth: number,
+) {
+  if (predictionDepth !== 0) {
+    const newAIPredictions: AIPrediction[] = [];
+
+    aiPredictions.forEach((prediction, index) => {
+      if (!prediction.completed) {
+        const newEmptyCases = getEmptyCases(prediction.arena);
+
+        for (let i = 0; i < newEmptyCases.length; i++) {
+          const newPrediction: AIPrediction = {
+            aiMove: {
+              y: prediction.aiMove.y,
+              x: prediction.aiMove.x,
+            },
+            arena: structuredClone(prediction.arena),
+            score: 0,
+            depth: prediction.depth + 1,
+            completed: false,
+          };
+
+          newPrediction.arena[newEmptyCases[i].y][newEmptyCases[i].x] = "o";
+
+          if (checkIfWin(newPrediction.arena) == "o") {
+            newPrediction.completed = true;
+            newPrediction.score = -10;
+          }
+
+          newAIPredictions.push(newPrediction);
+        }
+
+        prediction.completed = true;
+        aiPredictions.splice(index, 1);
+      } else {
+        newAIPredictions.push(prediction);
+      }
+    });
+
+    predictAIMoves(newAIPredictions, predictionDepth - 1);
+  } else {
+    rankPredictions(aiPredictions);
+  }
+}
+
+function predictAIMoves(
+  aiPredictions: AIPrediction[],
+  predictionDepth: number,
+) {
+  if (predictionDepth !== 0) {
+    const newAIPredictions: AIPrediction[] = [];
+
+    aiPredictions.forEach((prediction, index) => {
+      if (!prediction.completed) {
+        const newEmptyCases = getEmptyCases(prediction.arena);
+
+        for (let i = 0; i < newEmptyCases.length; i++) {
+          const newPrediction: AIPrediction = {
+            aiMove: {
+              y: prediction.aiMove.y,
+              x: prediction.aiMove.x,
+            },
+            arena: structuredClone(prediction.arena),
+            score: 0,
+            depth: prediction.depth + 1,
+            completed: false,
+          };
+
+          newPrediction.arena[newEmptyCases[i].y][newEmptyCases[i].x] = "x";
+
+          if (checkIfWin(newPrediction.arena) == "x") {
+            newPrediction.completed = true;
+            newPrediction.score = 10 - newPrediction.depth;
+          }
+
+          newAIPredictions.push(newPrediction);
+        }
+
+        prediction.completed = true;
+        aiPredictions.splice(index, 1);
+      } else {
+        newAIPredictions.push(prediction);
+      }
+    });
+
+    predictPlayerMoves(newAIPredictions, predictionDepth - 1);
+  } else {
+    rankPredictions(aiPredictions);
+  }
+}
+
+function rankPredictions(aiPredictions: AIPrediction[]) {
+  aiPredictions.sort((a, b) => {
+    return b.score - a.score;
+  });
+
+  rankedAIPredictions = aiPredictions;
+}
+
+function checkIfWin(arena: TicTacToeCase[][]): " " | "o" | "x" {
   const winPatterns = [
     [
       [0, 0],
@@ -257,9 +422,9 @@ function checkIfWin(): " " | "o" | "x" {
   let returnedValue: " " | "o" | "x" = " ";
 
   winPatterns.forEach((winPattern) => {
-    const firstCase = ticTacToeArena[winPattern[0][0]][winPattern[0][1]];
-    const secondCase = ticTacToeArena[winPattern[1][0]][winPattern[1][1]];
-    const thirdCase = ticTacToeArena[winPattern[2][0]][winPattern[2][1]];
+    const firstCase = arena[winPattern[0][0]][winPattern[0][1]];
+    const secondCase = arena[winPattern[1][0]][winPattern[1][1]];
+    const thirdCase = arena[winPattern[2][0]][winPattern[2][1]];
 
     if (firstCase !== " ") {
       if (firstCase === secondCase && firstCase === thirdCase) {
